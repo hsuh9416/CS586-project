550324cd8f3b83b2a071e42a4643de1ee1362f2a
Haeun Suh <hsuh7@hawk.iit.edu>
2024-04-18 23:20:35 -0500

    Modify to apply when no transaction but return cash

--- src/processor/Output/Action/Action12/ReturnCashImpl1.java
+++ src/processor/Output/Action/Action12/ReturnCashImpl1.java
@@ -10,8 +10,9 @@ public class ReturnCashImpl1 extends ReturnCash {
     @Override
     public void returnCash() {
         DataStore1 ds1 = (DataStore1) this.ds;
-        if(ds1.temp_c > 0){
-            int remain = ds1.cash - ds1.total;
+        if(ds1.w != 1){ // Only if not credit!
+            int rCash = (ds1.cash > 0) ? ds1.cash : ds1.temp_c;
+            int remain = rCash - ds1.total;
             System.out.println("[System] Remainder has been returned. Please remember to take your cash!");
             System.out.printf("[Info] Cash returned: $ %d\n", remain);
         }
--- src/processor/Output/Action/Action12/ReturnCashImpl2.java
+++ src/processor/Output/Action/Action12/ReturnCashImpl2.java
@@ -10,6 +10,7 @@ public class ReturnCashImpl2 extends ReturnCash {
     @Override
     public void returnCash() {
         DataStore2 ds2 = (DataStore2) this.ds;
+        float rCash = (ds2.cash > 0) ? ds2.cash : ds2.temp_cash;
         float remain = ds2.cash - ds2.total;
         System.out.println("[System] Remainder has been returned. Please remember to take your cash!");
         System.out.printf("[Info] Cash returned: $ %f\n", remain);
